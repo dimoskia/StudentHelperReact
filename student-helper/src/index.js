c140821c80d339cb5a124919997473df93448584
Kostadin <kostadinljatkoski@hotmail.com>
2020-01-15 17:06:07 +0100

    Folder restructuring

--- student-helper/src/index.js
+++ student-helper/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App/App';
 import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
