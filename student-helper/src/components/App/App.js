ff01acbee0803af3a63571597b70758e25a0c1e2
Kostadin <kostadinljatkoski@hotmail.com>
2020-01-15 19:16:44 +0100

    Folder restructuring

--- student-helper/src/components/App/App.js
+++ student-helper/src/components/App/App.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
+import {withRouter, Redirect, Route, Switch} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import './App.css';
+import Header from '../Header/Header';
+import Filters from '../Courses/Filters/Filters';
+import Container from "../Courses/Container/Container";
 
 class App extends Component {
 
     render() {
         return (
-            <div className='container'>
-                <h1>hello Student Helper :))</h1>
+            <div>
+                <BrowserRouter>
+                    <Header/>
+                    <div className="container">
+                        <Switch>
+                            <Route path={"/home"}>
+                                    <div className="row">
+                                        <Filters/>
+                                        <Container/>
+                                    </div>
+                            </Route>
+                        </Switch>
+                    </div>
+                </BrowserRouter>
             </div>
         );
     }
