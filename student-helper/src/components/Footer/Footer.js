2b47253814ed47425f34533406f0fa261c388d79
davinovski <hristijan_davinovski@hotmail.com>
2020-01-17 21:08:48 +0100

    Added Footer

---
+++ student-helper/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Footer = () =>{
+    return(
+        <footer className="Footer shadow-lg bg-primary text-center text-white">
+            <div className="lastFooter bg-primary text-center text-white py-3">
+                © 2020 Copyright: StudentHelper
+            </div>
+        </footer>
+    )
+};
+export default Footer;
